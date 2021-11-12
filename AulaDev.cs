df2ad55bb7ea3d0bd68df13de1ac7253ec9c9061
Luiz Omena <83364189+luizfilipeyep@users.noreply.github.com>
2021-11-12 11:01:07 -0300

    Add files via upload

---
+++ AulaDev.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AulaDev : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Teste!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		  if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Pressionou E, parabens mano");
+        }
+            if (Input.GetKeyUp(KeyCode.E))
+        {
+            Debug.Log("Despressionou E, parabens tambem mano");
+        }
+	}
+}
---
+++ AulaIf.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AulaIf : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (9 > 9) {
+			Debug.Log("Condição Verdadeira");
+		} else {
+			Debug.Log("Condição Falsa");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
